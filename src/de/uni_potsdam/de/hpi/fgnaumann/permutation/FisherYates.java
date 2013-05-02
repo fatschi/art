36fe06c6674d834ff9af86dd3f0714c3a5c849f8
Fabian Tschirschnitz <fabian.tschirschnitz@sap.com>
2013-05-02 14:28:46 +0200

    initial permutationgenrator commit

---
+++ src/de/uni_potsdam/de/hpi/fgnaumann/permutation/FisherYates.java
@@ -0,0 +1,35 @@
+package de.uni_potsdam.de.hpi.fgnaumann.permutation;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class FisherYates implements PermutationGenerator{
+
+	@Override
+	public int[] generateRandomPermutation(int size) {
+		int[] initialArray = initializeArray(size);
+		
+		Random r = new Random();
+	    for (int i = size - 1; i > 0; i--) {
+	        int index = r.nextInt(i);
+	        //swap
+	        int tmp = initialArray[index];
+	        initialArray[index] = initialArray[i];
+	        initialArray[i] = tmp;
+	    }
+	    return initialArray;
+	} 
+	
+	private int[] initializeArray(int size){
+		int[] initialArray = new int[size];
+		for(int i = 0; i < size; i++){
+			initialArray[i] = i;
+		}
+		return initialArray;
+	}
+	
+	public static void main(String[] args){
+		PermutationGenerator fisherYates = new FisherYates();
+		System.out.println(Arrays.toString(fisherYates.generateRandomPermutation(10)));
+	}
+}
---
+++ src/de/uni_potsdam/de/hpi/fgnaumann/permutation/PermutationGenerator.java
@@ -0,0 +1,5 @@
+package de.uni_potsdam.de.hpi.fgnaumann.permutation;
+
+public interface PermutationGenerator {
+	int[] generateRandomPermutation(int size);
+}
