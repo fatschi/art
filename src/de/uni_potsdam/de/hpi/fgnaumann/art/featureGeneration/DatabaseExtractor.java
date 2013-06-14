880bbb83981a3fb20da7686a07c65c8ab6b8b458
Nils Rethmeier <nils.rethmeier@student.hpi.uni-potsdam.de>
2013-06-14 17:07:57 +0200

    first version of noun extraction... lots to go

--- src/de/uni_potsdam/de/hpi/fgnaumann/art/featureGeneration/DatabaseExtractor.java
+++ src/de/uni_potsdam/de/hpi/fgnaumann/art/featureGeneration/DatabaseExtractor.java
@@ -1,9 +1,13 @@
 package de.uni_potsdam.de.hpi.fgnaumann.art.featureGeneration;
 
+import java.io.ObjectInputStream.GetField;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -14,13 +18,35 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
 
+import de.l3s.boilerpipe.BoilerpipeProcessingException;
+import de.l3s.boilerpipe.extractors.ArticleExtractor;
+
+/**
+ * 
+ * @author Nils R. (TF-IDF Extraction), Fabian T. (SQLITE USAGE)
+ *
+ */
 public class DatabaseExtractor {
 
 	static Options options = null;
 	static String connectionString = null;
+	static String outPath = null;
 	static Connection connection = null;
 	static ResultSet resultSet = null;
 	static Statement statement = null;
+	/**Tweet remover*/
+	static public Pattern tweets 		= Pattern.compile("Follow \\@\\w+");
+	/**Links*/
+	static public Pattern links 		= Pattern.compile("http\\S*\\s");
+	static public Pattern htmltags 		= Pattern.compile("\\<.*?>");
+	/**Too many whitespace remover*/
+	static public Pattern multispaces  = Pattern.compile(" +");
+	/**Multi to one line*/
+	static public Pattern multiline    = Pattern.compile("\n");
+	static public int PPLEN = new String("Previous Page").length();
+	static final ArticleExtractor FULLTEXT_EXTRACTOR = new ArticleExtractor();
+			
+    
 
 	public static void main(String[] args) {
 
@@ -57,9 +83,66 @@ public class DatabaseExtractor {
 					.getConnection(connectionString);
 			statement = connection.createStatement();
 			resultSet = statement
-					.executeQuery("SELECT id, cleaned_text FROM rss_article;");
+					.executeQuery("SELECT id, cleaned_text FROM rss_article LIMIT 100;");
+			
+			
+			// Create nounextraction object
+			NounExtractor nE = new NounExtractor();
+			HashMap<String, Long> collectionMap = new HashMap<String, Long>(10000, 0.95f);
 			while (resultSet.next()) {
-				System.out.println(resultSet.getString("id")+";"+resultSet.getString("cleaned_text"));
+//				System.out.println(resultSet.getString("id")+";"+resultSet.getString("cleaned_text"));
+				
+				// Pattern
+				// Ends with. previous page
+				String fulltext = resultSet.getString("cleaned_text");
+				if (fulltext.endsWith("Previous Page")) {
+					// rem the link text
+					fulltext = fulltext.substring(0,fulltext.length()-PPLEN-1);
+				}
+				
+				
+				// Rem tweet tags
+				Matcher lnks = links.matcher(fulltext);
+				fulltext = lnks.replaceAll(" ");
+				
+				// Erase HTML tags
+				Matcher tags = htmltags.matcher(fulltext);
+				fulltext = tags.replaceAll(" ");
+				
+				
+				// Rem tweet tags
+				Matcher twt = tweets.matcher(fulltext);
+				fulltext = twt.replaceAll("");
+				// Rem multiline
+				Matcher mline = multiline.matcher(fulltext);
+				fulltext = mline.replaceAll(" ");
+				// Rem multispace
+				Matcher mspace = multispaces.matcher(fulltext);
+				fulltext = mspace.replaceAll(" ").trim();
+				if (fulltext.contains("<")){
+					// some non-sense left overs
+					System.err.println(fulltext);
+				}
+				
+				// Extract nouns
+				long count = -1l;
+				boolean alreadyGlobal = false;
+				HashMap<String, Long> localNouns = nE.getNouns(fulltext);
+				String mostFrequentLocal = null;
+				for (String noun: localNouns.keySet()) {
+					noun = noun.toLowerCase();
+					if (collectionMap.containsKey(noun)) {
+						alreadyGlobal = true;	
+						Long oldCount = collectionMap.get(noun);
+						Long newOccur = localNouns.get(noun);
+						// Update global noun counts
+						collectionMap.put(noun, oldCount + newOccur);
+					} else {
+						// determine most frequent local noun
+						
+					} 
+				}
+				
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -82,12 +165,18 @@ public class DatabaseExtractor {
 				.withArgName("connectionString")
 				.hasArg()
 				.withDescription(
-						"the jdbc connection string, eg. 'jdbc:sqlite:/home/fabian/art/corpora/news.sqlite' or 'jdbc:postgresql://isfet.hpi.uni-potsdam.de:5432/art2013?user=art2013&password=nVesq3TfTmeqRkP' - DBMS type will be infered")
+						"the jdbc connection string, eg. 'jdbc:sqlite:/media/zwerg/art/ARTposgresdb/news.sqlite' or 'jdbc:postgresql://isfet.hpi.uni-potsdam.de:5432/art2013?user=art2013&password=nVesq3TfTmeqRkP' - DBMS type will be infered")
 				.create("connectionString");
 
 		Option help = new Option("help", "print this message");
+		Option out = OptionBuilder
+				.withArgName("out")
+				.hasArg()
+				.withDescription(
+						"the jdbc connection string, eg. 'jdbc:sqlite:/media/zwerg/art/ARTposgresdb/news.sqlite' or 'jdbc:postgresql://isfet.hpi.uni-potsdam.de:5432/art2013?user=art2013&password=nVesq3TfTmeqRkP' - DBMS type will be infered")
+				.create("out");
 
-		return cliOptions.addOption(connectionString).addOption(help);
+		return cliOptions.addOption(connectionString).addOption(help).addOption(out);
 	}
 
 	private static boolean evaluateCLIParameters(CommandLine line) {
@@ -98,6 +187,29 @@ public class DatabaseExtractor {
 			formatter.printHelp("DatabaseExtractor", options);
 			return false;
 		}
+		if (line.hasOption("out")) {
+			outPath = line.getOptionValue("out");
+		} else {
+			System.err.println("ERROR: NO OUTPATH SPECIFIED");
+			return false;
+		}
 		return true;
+	}
+	
+	/**
+	 * Extracts Fulltext from an HTML Document.
+	 * 
+	 * @param html
+	 * @return
+	 */
+	public String htmlToFulltext(String html) {
+		String fulltext = null;
+		try {
+			fulltext = FULLTEXT_EXTRACTOR.getText(html);
+		} catch (BoilerpipeProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return fulltext;
 	}
 }
